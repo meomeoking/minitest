6dd50bedc5171eb3a78882c431b72037fe34e6e3
meomeoking <dkdnfnd@handong.ac.kr>
2023-04-15 07:00:42 +0900

    Implement file load functions in manager.c

--- manager.c
+++ manager.c
@@ -56,10 +56,18 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+	fp = fopen("product.txt","rt");
+       	if(fp == NULL){
+	       	printf("=> 파일 없음\n");
+	       	return 0;
+       	}
+       	for(;;count++){
+	       	fscanf(fp,"%s",p[count].name);
+	       	if(feof(fp)) break;
+	       	fscanf(fp,"%d",&p[count].weight);
+	       	fscanf(fp,"%d",&p[count].price);
+       	}
+       	fclose(fp);
 	printf("=> 로딩 성공!\n");
 	return count;
 }
